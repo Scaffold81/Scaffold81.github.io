88cfb5ec9d653bcf1f81d4beb733e5268decd837
Владимир <scaffold81@mail.ru>
2024-12-10 11:08:37 +0700

    New build

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL Builds.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Scaffold81.github.io.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
